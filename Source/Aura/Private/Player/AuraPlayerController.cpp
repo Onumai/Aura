573b1a6e8636b8fa013870f5764869e808cfde5d
Onumai <onumai1992@googlemail.com>
2025-05-04 16:10:29 +0200

    Added Move function
    Implemented movement control based on player input direction. Extracted input axis values, calculated forward and right vectors using yaw rotation, and applied movement input to the controlled pawn accordingly.
    Also implemented the BP_AuraPlayerController and added the correct InputAction and the MappinContext.

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -3,6 +3,7 @@
 
 #include "Player/AuraPlayerController.h"
 #include "EnhancedInputSubsystems.h"
+#include <EnhancedInputComponent.h>
 
 AAuraPlayerController::AAuraPlayerController()
 {
@@ -28,4 +29,33 @@ void AAuraPlayerController::BeginPlay()
 	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
 	InputModeData.SetHideCursorDuringCapture(false);
 	SetInputMode(InputModeData);
+}
+
+void AAuraPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	
+	// Check if the input component is of type UEnhancedInputComponent
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent); // CastChecked ensures that the cast is valid and will assert if it fails.
+	
+	//Bind the MoveAction to the Move function using the Enhanced Input Component.
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AAuraPlayerController::Move);
+}
+
+void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
+{
+	const FVector2D InputAxisVector = InputActionValue.Get<FVector2D>();// Get the input axis vector from the action value.
+	const FRotator Rotation = GetControlRotation();// Get the control rotation of the player controller.
+	const FRotator YawRotation(0.f, Rotation.Yaw, 0.f); // Create a new rotation with only the yaw component.
+	// Calculate the forward direction based on the yaw rotation.
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);//GetUnitAxis returns a unit vector in the specified axis direction.
+	// Calculate the right direction based on the yaw rotation.
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y); 
+
+	if (APawn* ControlledPawn = GetPawn<APawn>())
+	{
+		// Add movement input to the controlled pawn based on the input axis vector and the calculated directions.
+		ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y); 
+		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
+	}
 }
