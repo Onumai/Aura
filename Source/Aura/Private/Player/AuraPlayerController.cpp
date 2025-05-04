a8b5f67c4858474a2a74a507a87ecad6a7b53bf0
Onumai <onumai1992@googlemail.com>
2025-05-04 15:33:39 +0200

    Added Mapping Context and some CursorSettings

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -2,4 +2,30 @@
 
 
 #include "Player/AuraPlayerController.h"
+#include "EnhancedInputSubsystems.h"
 
+AAuraPlayerController::AAuraPlayerController()
+{
+	// Enables replication for this property, ensuring it stays synchronized across server and clients.
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	check(AuraContext);
+	// Create the Enhanced Input Subsystem and add the mapping context.
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+	Subsystem->AddMappingContext(AuraContext, 0);
+
+	//CursorSettings
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+	// Set the input mode to game and UI to allow for mouse interaction with UI elements.
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+}
