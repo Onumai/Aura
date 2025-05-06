f126cc4051e265deeb720225acc810b9c78c4719
Onumai <onumai1992@googlemail.com>
2025-05-06 19:45:31 +0200

    Added ASC and AS
    Implemented AuraAbilitySystemComponent.cpp .h and AuraAtttributeSystemComponent.cpp .h

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -0,0 +1,5 @@
+// Copyright Patrick Haubner
+
+
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+
---
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -0,0 +1,5 @@
+// Copyright Patrick Haubner
+
+
+#include "AbilitySystem/AuraAttributeSet.h"
+
