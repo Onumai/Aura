f126992033dcc43c566170a0f0aa844fe8e6963b
Onumai <onumai1992@googlemail.com>
2025-05-25 18:25:40 +0200

    Asset Manager
    Implemented an AssetManger class. In it the GameplayTags gets Initialized as NativeGameplayTagss.

--- Source/Aura/Public/AuraGameplayTags.h
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -17,6 +17,9 @@ struct FAuraGameplayTags
 public:
 	static const FAuraGameplayTags& Get() { return GameplayTags; }
 	static void InitializeNativeGameplayTags();
+
+	FGameplayTag Attributes_Secondary_Armor;
+
 protected:
 
 private:
