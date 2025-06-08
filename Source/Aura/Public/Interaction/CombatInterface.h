307073e2aa4dd078416215cd04d67f6d51d25eea
Onumai <onumai1992@googlemail.com>
2025-06-08 13:11:16 +0200

    MotionWarping
    Plugin->MotionWarping. In BP_AuraCharacter added the Component. In the AM_Cast_FireBolt added the MotionWarping notify.
    In CombatInterface.h added the func UpdateFacingTarget with an FVector input and added the UFUNCTION to be implementable as an Event and Callable as an Function.
    In BP_AuraCharacter added the event and hooked the function AddOrUpdateWarpTargetFromLocation with the MotionWarpinComp as Target the the FVector as TargetLocation. In GA_FireBolt GetAvatarActorFromActorInfo->CastToCombatInterface and calling the UpdateFacingTarget function with the ProjectileTargetLocation as Target.

--- Source/Aura/Public/Interaction/CombatInterface.h
+++ Source/Aura/Public/Interaction/CombatInterface.h
@@ -7,7 +7,7 @@
 #include "CombatInterface.generated.h"
 
 // This class does not need to be modified.
-UINTERFACE(MinimalAPI)
+UINTERFACE(MinimalAPI, BlueprintType) //BlueprintType to enable calling the interface in BP
 class UCombatInterface : public UInterface
 {
 	GENERATED_BODY()
@@ -26,5 +26,8 @@ public:
 	virtual int32 GetPlayerLevel();
 
 	virtual FVector GetCombatSocketLocation();
+
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable) // Not virtual because BlueprintImplementableEvent
+	void UpdateFacingTarget(const FVector& Target);
 
 };
