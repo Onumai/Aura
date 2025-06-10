82f4a15a49baa71b3f27f53eb382bae588aef5df
Onumai <onumai1992@googlemail.com>
2025-06-10 19:25:31 +0200

    SetByCallerMagnitude
    Added in AuraGameplayTags Damage as a new GameplayTag.
    In AuraProjectileSpell::SpawnProjectile right before FinishSpawning we get the GameplayTags an call the UAbilitySystemBlueprintLibrary::AssingTagSetBayCallerMagnitude. So now we have a GameplayTag and an value for the damage. So now we set the damage amount in the GameplayAbility and GE_Damage will get the value that have the correct tag.

--- Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -7,6 +7,7 @@
 #include <AbilitySystemBlueprintLibrary.h>
 #include "AbilitySystemComponent.h"
 //#include <Kismet/KismetSystemLibrary.h>
+#include "Aura/Public/AuraGameplayTags.h"
 
 
 void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Handle, 
@@ -45,6 +46,9 @@ void UAuraProjectileSpell::SpawnProjectile(const FVector& ProjectileTargetLocati
 
 		const UAbilitySystemComponent* SourceASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(GetAvatarActorFromActorInfo());
 		const FGameplayEffectSpecHandle SpecHandle = SourceASC->MakeOutgoingSpec(DamageEffectClass, GetAbilityLevel(), SourceASC->MakeEffectContext());
+		
+		FAuraGameplayTags GameplayTags = FAuraGameplayTags::Get();
+		UAbilitySystemBlueprintLibrary::AssignTagSetByCallerMagnitude(SpecHandle, GameplayTags.Damage, 50.f);
 		Projectile->DamageEffectSpecHandle = SpecHandle;
 
 		Projectile->FinishSpawning(SpawnTransform);
