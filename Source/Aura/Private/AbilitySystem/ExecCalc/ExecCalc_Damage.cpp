673e26935ab9783f4707f0147873b2e992af45e7
Onumai <onumai1992@googlemail.com>
2025-06-15 11:14:46 +0200

    DamageExecutionCalculation
    Added ExecCalc_Damage class based on UGameplayEffectExecutionCalculation.
    Added Constrctor and an override func Execute_Implementation().
    Getting the ASC from Source and Target. Also the AvatarActor from Source and Target and gettting the GameplayEffectSpec.

---
+++ Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
@@ -0,0 +1,24 @@
+// Copyright Patrick Haubner
+
+
+#include "AbilitySystem/ExecCalc/ExecCalc_Damage.h"
+#include "AbilitySystemComponent.h"
+
+UExecCalc_Damage::UExecCalc_Damage()
+{
+
+}
+
+void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, 
+	FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
+{
+	const UAbilitySystemComponent* SourceASC = ExecutionParams.GetSourceAbilitySystemComponent();
+	const UAbilitySystemComponent* TargetASC = ExecutionParams.GetTargetAbilitySystemComponent();
+
+	AActor* SourceActor = SourceASC ? SourceASC->GetAvatarActor() : nullptr; 
+	AActor* TargetActor = TargetASC ? TargetASC->GetAvatarActor() : nullptr; 
+
+	const FGameplayEffectSpec& Spec = ExecutionParams.GetOwningSpec(); 
+
+
+}
