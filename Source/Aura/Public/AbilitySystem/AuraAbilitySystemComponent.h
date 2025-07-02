6236e9fbc805dbb11372cbd03326de044a2b5741
Onumai <onumai1992@googlemail.com>
2025-07-02 14:55:40 +0200

    Binding Widget Events to the Ability Info Delegate

--- Source/Aura/Public/AbilitySystem/AuraAbilitySystemComponent.h
+++ Source/Aura/Public/AbilitySystem/AuraAbilitySystemComponent.h
@@ -42,4 +42,7 @@ protected:
 	UFUNCTION(Client, Reliable)
 	void ClientEffectApplied(UAbilitySystemComponent* AbilitySystemComponent, const FGameplayEffectSpec& EffectSpec, FActiveGameplayEffectHandle ActiveEffectHandle);
 
+	virtual void OnRep_ActivateAbilities() override;
+
+	
 };
