6cad13f636ae2c2a380f549997b23078983b7213
Onumai <onumai1992@googlemail.com>
2025-09-03 14:21:22 +0200

    AuraBeamSpell
    Introduces UAuraBeamSpell, a new ability class for beam spells. Implements methods to store mouse hit data and owner controller, and updates the Electrocute ability asset.

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraBeamSpell.cpp
@@ -0,0 +1,25 @@
+// Copyright Patrick Haubner
+
+
+#include "AbilitySystem/Abilities/AuraBeamSpell.h"
+
+void UAuraBeamSpell::StoreMouseDataInfo(const FHitResult& HitResult)
+{
+	if (HitResult.bBlockingHit)
+	{
+		MouseHitLocation = HitResult.ImpactPoint;
+		MouseHitActor = HitResult.GetActor();
+	}
+	else
+	{
+		CancelAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true);
+	}
+}
+
+void UAuraBeamSpell::StoreMouseOwnerController()
+{
+	if (CurrentActorInfo)
+	{
+		OwnerPlayerController = CurrentActorInfo->PlayerController.Get();
+	}
+}
