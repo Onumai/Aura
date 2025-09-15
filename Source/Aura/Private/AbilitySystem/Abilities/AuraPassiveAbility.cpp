25d26648fe60b0a2e94809d0a7c06fbe4d682003
Onumai <onumai1992@googlemail.com>
2025-09-15 20:25:37 +0200

    Aura Passive Ability

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraPassiveAbility.cpp
@@ -0,0 +1,28 @@
+// Copyright Patrick Haubner
+
+
+#include "AbilitySystem/Abilities/AuraPassiveAbility.h"
+
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+
+void UAuraPassiveAbility::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+                                          const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+                                          const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	// If DeactivatePassiveAbility Delegate gets broadcast from AuraASC ReceiveDeactivate gets triggertd
+	if (UAuraAbilitySystemComponent* AuraASC = Cast<UAuraAbilitySystemComponent>(UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(GetAvatarActorFromActorInfo())))
+	{
+		AuraASC->DeactivatePassiveAbility.AddUObject(this, &UAuraPassiveAbility::ReceiveDeactivate);
+	}
+}
+
+void UAuraPassiveAbility::ReceiveDeactivate(const FGameplayTag& AbilityTag)
+{
+	if (AbilityTags.HasTagExact(AbilityTag))
+	{
+		EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
+	}
+}
