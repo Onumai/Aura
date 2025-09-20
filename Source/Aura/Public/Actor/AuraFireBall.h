0d34d246372be94c89110c13cbfbd45baeec3cf9
Onumai <onumai1992@googlemail.com>
2025-09-20 13:26:33 +0200

    Causing FireBall Damage

--- Source/Aura/Public/Actor/AuraFireBall.h
+++ Source/Aura/Public/Actor/AuraFireBall.h
@@ -24,5 +24,10 @@ public:
 protected:
 	virtual void BeginPlay() override;
 	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override; // Called when the projectile overlaps with another actor
-	
+
+	UFUNCTION(BlueprintCallable)
+	void EmptyIgnoreList();
+     
+	UPROPERTY(BlueprintReadWrite)
+	TArray<AActor*> IgnoreList;
 };
