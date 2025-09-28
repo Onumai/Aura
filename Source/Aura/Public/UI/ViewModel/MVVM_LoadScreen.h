667d6beab50b61fa574c0d31d65ef6ca6a175558
Onumai <onumai1992@googlemail.com>
2025-09-28 19:17:38 +0200

    Enabling Play and Delete Buttons
    Introduces a BlueprintAssignable SlotSelected multicast delegate to UMVVM_LoadScreen, broadcasting when a slot is selected. Updates SelectSlotButtonPressed to trigger the delegate, enabling UI event handling for slot selection.

--- Source/Aura/Public/UI/ViewModel/MVVM_LoadScreen.h
+++ Source/Aura/Public/UI/ViewModel/MVVM_LoadScreen.h
@@ -6,6 +6,9 @@
 #include "MVVMViewModelBase.h"
 #include "MVVM_LoadScreen.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FSlotSelected);
+
+
 class UMVVM_LoadSlot;
 /**
  * 
@@ -18,6 +21,9 @@ class AURA_API UMVVM_LoadScreen : public UMVVMViewModelBase
 public:
 
 	void InitializeLoadSlots();
+
+	UPROPERTY(BlueprintAssignable)
+	FSlotSelected SlotSelected;
 	
 	UPROPERTY(EditDefaultsOnly, Category = "LoadScreen")
 	TSubclassOf<UMVVM_LoadSlot> LoadSlotViewModelClass;
