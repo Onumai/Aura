db6e89d6f355f90b8818fae79ab5893af224e8b0
Onumai <onumai1992@googlemail.com>
2025-10-12 12:14:42 +0200

    Saving the Map Name
    Introduces a MapName property to ULoadScreenSaveGame and ensures it is saved and loaded with slot data. This allows tracking the map associated with each save slot.

--- Source/Aura/Private/UI/ViewModel/MVVM_LoadScreen.cpp
+++ Source/Aura/Private/UI/ViewModel/MVVM_LoadScreen.cpp
@@ -89,5 +89,6 @@ void UMVVM_LoadScreen::LoadData()
 		LoadSlot.Value->LoadSlotStatus = SaveSlotStatus;
 		LoadSlot.Value->SetPlayerName(PlayerName);
 		LoadSlot.Value->InitializeSlot();
+		LoadSlot.Value->SetMapName(SaveObject->MapName);
 	}
 }
